b08c3d14b1449a6fa8ff53e7e0e50ae31adb88ec
naelmessaoudene <naelmessaoudene@gmail.com>
2020-06-13 23:53:27 +0200

    remove default styles

--- pages/index.vue
+++ pages/index.vue
@@ -37,18 +37,3 @@ export default {
   }
 }
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
