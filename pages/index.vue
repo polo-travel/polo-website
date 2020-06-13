d659462effb99859a0e8be1cc13805eedf34ed77
naelmessaoudene <naelmessaoudene@gmail.com>
2020-06-13 23:37:54 +0200

    wip on style's architecture & create font-faces

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
         polo website
       </h1>
       <h2 class="subtitle">
-        polo website
+        polo !!!!!! thewebsite
       </h2>
       <div class="links">
         <a
@@ -46,24 +46,6 @@ export default {
   justify-content: center;
   align-items: center;
   text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
 }
 
 .links {
