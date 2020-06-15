af253741befea281c1e118983dab23508d6afb46
naelmessaoudene <naelmessaoudene@gmail.com>
2020-06-15 19:05:07 +0200

    add video

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="container">
     <home></home>
+    <teaser></teaser>
   </div>
 </template>
 
 <script>
 import Home from '~/components/Home/Home.vue'
+import Teaser from '~/components/Video/Video.vue'
 
 export default {
   components: {
-    Home
+    Home,
+    Teaser
   }
 }
 </script>
