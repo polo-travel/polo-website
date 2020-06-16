883fa1c746ec9d759b149b74cd87ef4237c61830
naelmessaoudene <naelmessaoudene@gmail.com>
2020-06-16 19:18:06 +0200

    add basic feature components

--- components/Features/Feature.vue
+++ components/Features/Feature.vue
@@ -3,17 +3,27 @@
         <rounded title="Une expérience à offrir
 ou à s’offrir" imgUrl="app-travel.svg" description="Faites le plus incroyable des cadeaux et offrez à vos proches
 une aventure pleine de surprises et d’activités insolites."></rounded>
+
+        <basic title="Un itinéraire étonnant" imgUrl="app-destination.svg" description="Découvrez vos activités surprises tout au long de votre voyage
+en suivant les indications de Polo qui sera votre guide."></basic>
+
+        <rounded title="Une expérience à offrir
+ou à s’offrir" imgUrl="app-travel.svg" description="Faites le plus incroyable des cadeaux et offrez à vos proches
+une aventure pleine de surprises et d’activités insolites."></rounded>
+
     </div>
 </template>
 
 <script>
 
     import rounded from '~/components/Features/roundedFeature.vue'
+    import basic from '~/components/Features/defaultFeature.vue'
 
     export default {
         name: "Feature",
         components:{
-            rounded
+            rounded,
+            basic
         }
     }
 </script>
