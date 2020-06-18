0b72482b780d0dfbd67fecd7b479402aa7f1bd54
naelmessaoudene <naelmessaoudene@gmail.com>
2020-06-18 21:53:57 +0200

    wip on responsive team page

--- components/Navigation/Navigation.vue
+++ components/Navigation/Navigation.vue
@@ -2,7 +2,7 @@
     <div class="navigation">
         <logo></logo>
         <ul class="navigation-list">
-            <li class="navigation-item">Polo</li>
+            <li class="navigation-item"><nuxt-link to="/">Polo</nuxt-link></li>
             <li class="navigation-item"> <nuxt-link to="/team">La team</nuxt-link></li>
             <li class="navigation-item">Contact</li>
         </ul>
--- pages/team/index.vue
+++ pages/team/index.vue
@@ -1,17 +1,54 @@
 <template>
-    <div class="team">
-        <navigation>s</navigation>
-        <h2>EKIP A FOND</h2>
+    <div class="container">
+        <div class="team">
+            <navigation></navigation>
+            <div class="contentWrapper">
+                <h1 class="team-title">La team Polo</h1>
+                <p class="team-description">Nous sommes 4 étudiants en Bachelor Design Interactif à l’Ecole des Gobelins d’Annecy. <br>
+                    Nous avons réalisé ce projet dans le cadre de notre projet de fin d’année sur le thème de la mobilité connectée.</p>
+                <div class="membersWrapper">
+                    <div class="team-user">
+                        <img src="../../static/img/killian.svg" alt="">
+                        <span class="team-user--name">Killian Sowa</span>
+                        <span class="team-user--role">Développeur</span>
+                    </div>
+                    <div class="team-user">
+                        <img src="../../static/img/lea.svg" alt="">
+                        <span class="team-user--name">Léa Pradel</span>
+                        <span class="team-user--role">Designer</span>
+                    </div>
+                    <div class="team-user">
+                        <img src="../../static/img/melanie.svg" alt="">
+                        <span class="team-user--name">Mélanie Dalla-Vecchia</span>
+                        <span class="team-user--role">Designer</span>
+                    </div>
+                    <div class="team-user">
+                        <img src="../../static/img/nael.svg" alt="">
+                        <span class="team-user--name">Naël Messaoudene</span>
+                        <span class="team-user--role">Développeur</span>
+                    </div>
+
+                    <img class="team-dashed" src="../../static/img/dashed-line.svg" alt="">
+
+                </div>
+                <img class="team-compass" src="../../static/img/compass.svg" alt="">
+            </div>
+        </div>
+        <app-footer class="footer-team"></app-footer>
     </div>
+
+
 </template>
 
 <script>
     import Navigation from '~/components/Navigation/Navigation.vue'
+    import appFooter from '~/components/Footer/Footer.vue'
 
     export default {
         name: "index.vue",
         components:{
-            Navigation
+            Navigation,
+            appFooter
         }
     }
 </script>
