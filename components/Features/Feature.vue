fae5a23ae319f55ad9acb698c9742e0e979154ff
naelmessaoudene <naelmessaoudene@gmail.com>
2020-06-23 08:54:19 +0200

    change text

--- components/Features/Feature.vue
+++ components/Features/Feature.vue
@@ -7,9 +7,8 @@ une aventure pleine de surprises et d’activités insolites." :display=false></
         <basic title="Un itinéraire étonnant" imgUrl="app-destination.svg" description="Découvrez vos activités surprises tout au long de votre voyage
 en suivant les indications de Polo qui sera votre guide." bgImg="destination.svg" ></basic>
 
-        <rounded title="Une expérience à offrir
-ou à s’offrir" imgUrl="app-travel.svg" description="Faites le plus incroyable des cadeaux et offrez à vos proches
-une aventure pleine de surprises et d’activités insolites." v-bind:class="{ red: isActive}" :display=true ></rounded>
+        <rounded title="Une aventure personnalisée" imgUrl="app-travel.svg" description="Grâce à un questionnaire simplifié, Polo vous crée un voyage insolite
+adapté à votre profil d’aventurier." v-bind:class="{ red: isActive}" :display=true ></rounded>
 
         <basic title="Un itinéraire étonnant" imgUrl="app-polowers.png" description="Découvrez vos activités surprises tout au long de votre voyage
 en suivant les indications de Polo qui sera votre guide." bgImg="polowers.svg" v-bind:class="{ basic: isActive}" ></basic>
